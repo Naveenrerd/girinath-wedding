bda33e8e0182b05d313fd724e4c13599f14bdb11
Naveen Kumar <naveen@cloudthing.com>
2022-05-05 22:50:23 +0530

    All new updates

--- src/pages/components/freecoupon.tsx
+++ src/pages/components/freecoupon.tsx
@@ -1,28 +1,57 @@
-import { Box, Button, Dialog } from "@mui/material";
+import { Box, Button, Dialog, IconButton } from "@mui/material";
 import { useState } from "react";
-import n1 from "../../assets/n1.jpg";
+import n1 from "../../assets/n1.png";
+import CloseIcon from "@mui/icons-material/Close";
 
 const FreeCoupon: React.FC = () => {
-	const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-	const handleClickOpen = () => {
-		setOpen(true);
-	};
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-	const handleClose = (value: string) => {
-		setOpen(false);
-	};
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-	return (
-		<Box mt={5}>
-			<Button variant="contained" onClick={handleClickOpen}>
-				Click here for ₹500 free voucher
-			</Button>
-			<Dialog onClose={handleClose} open={open} sx={{ backgroundColor: "white" }}>
-				<img src={n1} alt="n1" />
-			</Dialog>
-		</Box>
-	);
+  return (
+    <Box mt={5}>
+      <Button
+        variant="contained"
+        onClick={handleClickOpen}
+        sx={{ backgroundColor: "#fa5b3d" }}
+      >
+        Click here for ₹500 free voucher
+      </Button>
+      <Dialog onClose={handleClose} open={open}>
+        <Box
+          sx={{
+            position: "relative",
+            overflow: "hidden",
+            display: "flex",
+          }}
+        >
+          <img src={n1} alt="n1" height="300px" />
+          <IconButton
+            aria-label="close"
+            sx={{
+              position: "absolute",
+              top: 2,
+              right: 2,
+              color: "black",
+            }}
+            onClick={handleClose}
+          >
+            <CloseIcon
+              sx={{
+                fontSize: 30,
+              }}
+            />
+          </IconButton>
+        </Box>
+      </Dialog>
+    </Box>
+  );
 };
 
 export default FreeCoupon;
